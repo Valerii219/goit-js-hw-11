a43c700ff11d2e8ada71b181e07517a89a71f9cb
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-06-02 21:52:23 +0200

    save changes

--- src/index.js
+++ src/index.js
@@ -65,30 +65,26 @@ function clearContainer(){
 }
 function createMarkup(arr){
     return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
-    ` 
-    <div class="photo-card">
-    <a class="gallery__link" href="${webformatURL}">
-<img
-    class="gallery__image"
-    src="${largeImageURL}" 
-        alt="${tags}" 
-        loading="lazy"
-/> 
-    <div class="info">
-      <p class="info-item">
-        <b>Likes:${likes}</b>
-      </p>
-      <p class="info-item">
-        <b>Views:${views}</b>
-      </p>
-      <p class="info-item">
-        <b>Comments:${comments}</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads:${downloads}</b>
-      </p>
-    </div>
-  </div> `).join()
+    `<div class="photo-card">
+    <a class="gallery__link" href="${largeImageURL}">
+      <img class = img-card src="${webformatURL}" alt="${tags}" loading="lazy"  data-source="${largeImageURL}"/></a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes:${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views:${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments:${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads:${downloads}</b>
+        </p>
+      </div>
+    </div>`)
+  .join('')
 }
+
 
 
