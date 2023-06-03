19e6d30bede0a8c63440ee3a165d9869c293eb05
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-06-03 18:44:55 +0200

    save changes

--- src/newSerwices.js
+++ src/newSerwices.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import LoadButton from './buttonOptions';
 const url = 'https://pixabay.com/api';
     const API_KEY = '36861352-2474982a97ff1b570eda1c4c2';
     const END_POINT ='image_type=photo&orientation=horizontal&safesearch=true'
-  
+    const LoadMoreButton = new LoadButton({
+      selector: '[data-action="load"]',
+      hidden:true,
+    })
     const axiosInstance = axios.create(
         {
         baseURL: url,
@@ -15,14 +20,24 @@ export default class NewServices{
         this.nameInput = "";
         this.page = 1;
         this.totalHitsResult = 0;
+        this.totalPagesResult = 0;
     }
     
     async fetchArticles() {
         try {
           const response = await axiosInstance.get(`?key=${API_KEY}&q=${this.nameInput}&${END_POINT}&per_page=40&page=${this.page}`);
           const result = response.data;
+          console.log(result);
           this.totalHitsResult = result.totalHits;
+          this.totalPagesResult = Math.ceil(result.totalHits / 40);
           this.page += 1;
+          if (this.page <= this.totalPagesResult) {
+            console.log(this.totalPagesResult);
+            console.log(this.page);
+          } else {
+            LoadMoreButton.hide();
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results")
+          } 
           return result.hits;
         } catch (error) {
           console.error(error);
