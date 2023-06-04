d35257ce7d2cfc9f0d016c2760e05fd7cc1a6dbb
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-06-04 20:02:48 +0200

    іі

--- src/index.js
+++ src/index.js
@@ -1,79 +1,88 @@
 import Notiflix from 'notiflix';
 import NewServices from './newSerwices';
 import LoadButton from './buttonOptions';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const form  = document.querySelector('.search-form');
+const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const lightbox = new SimpleLightbox(".gallery__link");
+const lightbox = new SimpleLightbox('.gallery__link');
 
-const newSerwice = new NewServices({
-});
+const newSerwice = new NewServices({});
 
 const LoadMoreButton = new LoadButton({
   selector: '[data-action="load"]',
-  hidden:true,
- 
-})
+  hidden: true,
+});
 
 form.addEventListener('submit', onSearch);
 LoadMoreButton.refs.button.addEventListener('click', onLoad);
 
-function onSearch(e){
-  LoadMoreButton.hide()
-e.preventDefault();
-clearContainer()
-newSerwice.query = e.currentTarget.elements.searchQuery.value;
-if(newSerwice.query === ""){
-  return Notiflix.Notify.info('You need to enter certain data')
-}
-LoadMoreButton.show();
-LoadMoreButton.disable();
-newSerwice.resetPage();
-newSerwice.fetchArticles().then(hits =>
-  {clearContainer();
+function onSearch(e) {
+  LoadMoreButton.hide();
+  e.preventDefault();
+  clearContainer();
+  newSerwice.query = e.currentTarget.elements.searchQuery.value;
+  if (newSerwice.query.trim().length === 0) {
+    return Notiflix.Notify.info('You need to enter certain data');
+  }
+  LoadMoreButton.show();
+  LoadMoreButton.disable();
+  newSerwice.resetPage();
+  newSerwice.fetchArticles().then(hits => {
+    clearContainer();
     addMarkupOnPage(hits);
     LoadMoreButton.enable();
     const totalHits = newSerwice.totalHits();
-    if(newSerwice.totalHits() > 0){
+    if (newSerwice.totalHits() > 0) {
       Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     }
     lightbox.refresh();
-    const { height: cardHeight } = 
-    gallery.firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } =
+      gallery.firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-});
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   });
 }
 
-function onLoad(){
+function onLoad() {
   LoadMoreButton.disable();
-  newSerwice.fetchArticles().then(hits =>{
-      addMarkupOnPage(hits);
-      LoadMoreButton.enable();
-    });
+  newSerwice.fetchArticles().then(hits => {
+    addMarkupOnPage(hits);
+    LoadMoreButton.enable();
+  });
 }
-function addMarkupOnPage(hits){
-  gallery.insertAdjacentHTML('beforeend',createMarkup(hits))
-    if(hits.length === 0){
-      Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
-      LoadMoreButton.hide();
-   
-    }
-    lightbox.refresh();
+function addMarkupOnPage(hits) {
+  gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+  if (hits.length === 0) {
+    Notiflix.Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    LoadMoreButton.hide();
+  }
+  lightbox.refresh();
 }
 
-function clearContainer(){
-    gallery.innerHTML = "";
+function clearContainer() {
+  gallery.innerHTML = '';
 }
-function createMarkup(arr){
-    return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
-    `<div class="photo-card">
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="photo-card">
         <a class="gallery__link" href="${largeImageURL}">
       <img class = img-card src="${webformatURL}" alt="${tags}" loading="lazy"  data-source="${largeImageURL}"/></a>
       <div class="info">
@@ -90,6 +99,7 @@ function createMarkup(arr){
           <b class = "bb">Downloads<span class = span-opt>${downloads}</span></b>
         </p> 
       </div>
-    </div>`)
-  .join('')
+    </div>`
+    )
+    .join('');
 }
